6ff4c2da284b52e2e7cc1398712a8437345d447e
vagrant <vagrant@homestead>
2017-10-09 04:10:32 +0000

    add delete

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -62,6 +62,13 @@
 
                                 <td>
                                     <!-- TODO: Delete Button -->
+                                    <form action="{{ url('task/'.$task->id) }}" method="POST">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+                                        <button type="submit" class="btn btn-danger">
+                                            <i class="fa fa-trash"></i> Delete
+                                        </button>
+                                    </form>
                                 </td>
                             </tr>
                         @endforeach
--- routes/web.php
+++ routes/web.php
@@ -51,4 +51,6 @@ Route::post('/task', function (Request $request) {
  */
 Route::delete('/task/{task}', function (Task $task) {
     //
+    $task->delete();
+    return redirect('/tasks');
 });
